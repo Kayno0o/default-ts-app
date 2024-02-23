6507fe372f850542b27967adb5c54ea0fdd98f5e
Kevyn Fyleyssant <kevyn.fyleyssant@gmail.com>
2024-02-23 22:19:40 +0100

    feat: update deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,8 @@
-module.exports = {
-  extends: [
-    '@antfu',
-  ],
+import antfu from '@antfu/eslint-config'
+
+export default antfu({
   rules: {
     'no-console': 'off',
+    'no-extend-native': 'off',
   },
-}
+})
